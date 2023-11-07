68d0394146fde401dc472e238079f7a0ac1c914f
Jesus <jsr20xx@gmail.com>
2023-11-07 13:27:31 +0100

    Components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const name = <h1>Jesus</h1>;
+  const age = <h2>21</h2>;
+  const email = <h2>jesus@jesus.com</h2>;
+  return (
+    <div className="App">
+      <div>
+        <User />
+      </div>
+    </div>
+  );
 }
+
+const User = () => {
+  return (
+    <div>
+      <h1>Jesus</h1>
+      <h1>23</h1>
+      <h1>jesus@jesus.com</h1>
+    </div>
+  );
+};
 
 export default App;

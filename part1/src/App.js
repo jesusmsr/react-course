d338234554f4c282511a3dd4999275cd0def182f
Jesus <jsr20xx@gmail.com>
2023-11-07 13:29:40 +0100

    Props

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,18 +7,20 @@ function App() {
   return (
     <div className="App">
       <div>
-        <User />
+        <User name="jesus" age={23} email="jesus@jesus.com" />
+        <User name="david" age={23} email="david@david.com" />
+        <User name="laura" age={23} email="laura@laura.com" />
       </div>
     </div>
   );
 }
 
-const User = () => {
+const User = (props) => {
   return (
     <div>
-      <h1>Jesus</h1>
-      <h1>23</h1>
-      <h1>jesus@jesus.com</h1>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
     </div>
   );
 };
